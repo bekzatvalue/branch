9336fa2137efc8cb35e6c139ccb05122839a50d9
bekzatrazhapaliuulu <bekzat.razhapaliuulu.12345@gamail.com>
2022-01-20 13:21:48 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (20 > 18) {
+            System.out.println("20 is greater than 18");
+        }
+
+
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+            if (i == 4) {
+                break;
+            }
+        }
+    }
+}
